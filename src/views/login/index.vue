d20380808c26b84c49f1c131e8e57369bee23cbf
juhui <330132662@qq.com>
2023-02-22 18:01:20 +0800

    哦豁

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -14,7 +14,7 @@
 
       <el-form-item prop="username">
         <span class="svg-container">
-          <svg-icon icon-class="user" />
+          <svg-icon icon-class="user"/>
         </span>
         <el-input
           ref="username"
@@ -29,7 +29,7 @@
 
       <el-form-item prop="password">
         <span class="svg-container">
-          <svg-icon icon-class="password" />
+          <svg-icon icon-class="password"/>
         </span>
         <el-input
           :key="passwordType"
@@ -54,7 +54,7 @@
         type="primary"
         style="width: 100%; margin-bottom: 30px"
         @click.native.prevent="handleLogin"
-        >Login
+      >Login
       </el-button>
 
       <div class="tips">
@@ -66,234 +66,242 @@
 </template>
 
 <script>
-import { validUsername } from "@/utils/validate";
-import request from "@/utils/request";
-import { MessageBox, Message } from "element-ui";
-export default {
-  name: "Login",
-  data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error("Please enter the correct user name"));
-      } else {
-        callback();
-      }
-    };
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error("The password can not be less than 6 digits"));
-      } else {
-        callback();
-      }
-    };
-    return {
-      accessToken: "",
-      loginForm: {
-        username: "admin",
-        password: "",
-      },
-      loginRules: {
-        username: [
-          { required: true, trigger: "blur", validator: validateUsername },
-        ],
-        password: [
-          { required: true, trigger: "blur", validator: validatePassword },
-        ],
-      },
-      loading: false,
-      passwordType: "password",
-      redirect: undefined,
-    };
-  },
-  watch: {
-    $route: {
-      handler: function (route) {
-        this.redirect = route.query && route.query.redirect;
-      },
-      immediate: true,
-    },
-  },
-  mounted() {
-    var that = this;
-    console.log(process.env.VUE_APP_BASE_API)
-    // this.getToken()
-    //   .then((response) => {
-    //     that.accessToken = response.data;
-    //     console.warn("mounted...", that.accessToken);
-    //   })
-    //   .catch((err) => {
-    //     console.warn(err);
-    //   });
-  },
-  methods: {
-    getToken() {
-      return request({
-        url: "/index/token_get",
-        method: "get",
-      });
-    },
-    showPwd() {
-      if (this.passwordType === "password") {
-        this.passwordType = "";
-      } else {
-        this.passwordType = "password";
-      }
-      this.$nextTick(() => {
-        this.$refs.password.focus();
-      });
-    },
-    handleLogin() {
-      this.loading = true;
-      var that = this;
-      this.$refs.loginForm.validate((valid) => {
-        if (valid) {
-          return request({
-            url: "/index/vuelogin",
-            method: "post",
-            data: {
-              username: this.loginForm.username,
-              password: this.loginForm.password,
-              __token__: that.accessToken,
-            },
-          })
-            .then((response) => {
-              console.warn("...", response);
-              if (response.code == 1) {
-                this.$router.push({ path: "/" });
-              } else {
-                //  提示用户 错误信息
-                Message({
-                  message: response.msg,
-                  type: "error",
-                  duration: 5 * 1000,
-                });
-              }
-              this.loading = false;
-            })
-            .catch((err) => {
-              this.loading = false;
+  import {validUsername} from "@/utils/validate";
+  import request from "@/utils/request";
+  import {MessageBox, Message} from "element-ui";
 
-              console.warn(err);
-            });
+  export default {
+    name: "Login",
+    data() {
+      const validateUsername = (rule, value, callback) => {
+        if (!validUsername(value)) {
+          callback(new Error("Please enter the correct user name"));
         } else {
-          this.loading = false;
-
-          console.log("error submit!!");
-          return false;
+          callback();
         }
-      });
+      };
+      const validatePassword = (rule, value, callback) => {
+        if (value.length < 6) {
+          callback(new Error("The password can not be less than 6 digits"));
+        } else {
+          callback();
+        }
+      };
+      return {
+        accessToken: "",
+        loginForm: {
+          username: "admin",
+          password: "888888",
+        },
+        loginRules: {
+          username: [
+            {required: true, trigger: "blur", validator: validateUsername},
+          ],
+          password: [
+            {required: true, trigger: "blur", validator: validatePassword},
+          ],
+        },
+        loading: false,
+        passwordType: "password",
+        redirect: undefined,
+      };
     },
-  },
-};
+    watch: {
+      $route: {
+        handler: function (route) {
+          this.redirect = route.query && route.query.redirect;
+        },
+        immediate: true,
+      },
+    },
+    mounted() {
+      var that = this;
+      console.log(process.env.VUE_APP_BASE_API)
+      // this.getToken()
+      //   .then((response) => {
+      //     that.accessToken = response.data;
+      //     console.warn("mounted...", that.accessToken);
+      //   })
+      //   .catch((err) => {
+      //     console.warn(err);
+      //   });
+    },
+    methods: {
+      getToken() {
+        return request({
+          url: "/index/token_get",
+          method: "get",
+        });
+      },
+      showPwd() {
+        if (this.passwordType === "password") {
+          this.passwordType = "";
+        } else {
+          this.passwordType = "password";
+        }
+        this.$nextTick(() => {
+          this.$refs.password.focus();
+        });
+      },
+      handleLogin() {
+        this.loading = true;
+        var that = this;
+        this.$refs.loginForm.validate((valid) => {
+          if (valid) {
+            return request({
+              url: "/index/vuelogin",
+              method: "post",
+              data: {
+                username: this.loginForm.username,
+                password: this.loginForm.password,
+                __token__: that.accessToken,
+              },
+            })
+              .then((response) => {
+                // console.warn("...", response);
+                if (response.code === 1) {
+                  Message({
+                    message: response.msg,
+                    type: "success",
+                    duration: 5 * 1000,
+                  });
+                  // that.$router.push({ path: "/" , query: this.otherQuery});
+                  console.log(this.redirect)
+                  this.$router.push({path: this.redirect || '/'})
+                } else {
+                  //  提示用户 错误信息
+                  Message({
+                    message: response.msg,
+                    type: "error",
+                    duration: 5 * 1000,
+                  });
+                }
+                this.loading = false;
+              })
+              .catch((err) => {
+                this.loading = false;
+
+                console.warn(err);
+              });
+          } else {
+            this.loading = false;
+
+            console.log("error submit!!");
+            return false;
+          }
+        });
+      },
+    },
+  };
 </script>
 
 <style lang="scss">
-/* 修复input 背景不协调 和光标变色 */
-/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
+  /* 修复input 背景不协调 和光标变色 */
+  /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg: #283443;
-$light_gray: #fff;
-$cursor: #fff;
+  $bg: #283443;
+  $light_gray: #fff;
+  $cursor: #fff;
 
-@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
-  .login-container .el-input input {
-    color: $cursor;
-  }
-}
-
-/* reset element-ui css */
-.login-container {
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-
-    input {
-      background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
-
-      &:-webkit-autofill {
-        box-shadow: 0 0 0px 1000px $bg inset !important;
-        -webkit-text-fill-color: $cursor !important;
-      }
+  @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+    .login-container .el-input input {
+      color: $cursor;
     }
   }
 
-  .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
+  /* reset element-ui css */
+  .login-container {
+    .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: 0px;
+        -webkit-appearance: none;
+        border-radius: 0px;
+        padding: 12px 5px 12px 15px;
+        color: $light_gray;
+        height: 47px;
+        caret-color: $cursor;
+
+        &:-webkit-autofill {
+          box-shadow: 0 0 0px 1000px $bg inset !important;
+          -webkit-text-fill-color: $cursor !important;
+        }
+      }
+    }
+
+    .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      background: rgba(0, 0, 0, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
   }
-}
 </style>
 
 <style lang="scss" scoped>
-$bg: #2d3a4b;
-$dark_gray: #889aa4;
-$light_gray: #eee;
+  $bg: #2d3a4b;
+  $dark_gray: #889aa4;
+  $light_gray: #eee;
 
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
+  .login-container {
+    min-height: 100%;
+    width: 100%;
+    background-color: $bg;
     overflow: hidden;
-  }
 
-  .tips {
-    font-size: 14px;
-    color: #fff;
-    margin-bottom: 10px;
+    .login-form {
+      position: relative;
+      width: 520px;
+      max-width: 100%;
+      padding: 160px 35px 0;
+      margin: 0 auto;
+      overflow: hidden;
+    }
 
-    span {
-      &:first-of-type {
-        margin-right: 16px;
+    .tips {
+      font-size: 14px;
+      color: #fff;
+      margin-bottom: 10px;
+
+      span {
+        &:first-of-type {
+          margin-right: 16px;
+        }
       }
     }
-  }
 
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
+    .svg-container {
+      padding: 6px 5px 6px 15px;
+      color: $dark_gray;
+      vertical-align: middle;
+      width: 30px;
+      display: inline-block;
+    }
 
-  .title-container {
-    position: relative;
+    .title-container {
+      position: relative;
 
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
+      .title {
+        font-size: 26px;
+        color: $light_gray;
+        margin: 0px auto 40px auto;
+        text-align: center;
+        font-weight: bold;
+      }
+    }
+
+    .show-pwd {
+      position: absolute;
+      right: 10px;
+      top: 7px;
+      font-size: 16px;
+      color: $dark_gray;
+      cursor: pointer;
+      user-select: none;
     }
   }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-}
 </style>
